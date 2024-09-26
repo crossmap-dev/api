eec87a3ba368d79368e393815d6a8184dbb1dc82
Mike Taylor <mike.charles.taylor@gmail.com>
2024-09-26 13:34:47 -0600

    WIP: API authentication

--- src/CROSSMAP/Server/Auth.hs
+++ src/CROSSMAP/Server/Auth.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module CROSSMAP.Server.Auth
   ( UserSignature(..)
   , SessionSignature(..)
@@ -9,17 +10,40 @@ module CROSSMAP.Server.Auth
   ) where
 
 import Control.Monad.IO.Class (liftIO)
-import Network.Wai (Request)
+import Crypto.Sign.Ed25519 (PublicKey(..))
+import Data.ByteString
+import Data.CaseInsensitive (original)
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
+import Data.UUID (UUID, fromText)
+import Network.HTTP.Types
+import Network.Wai
 import Servant
 import Servant.Server.Experimental.Auth
 
 
 data UserSignature = UserSignature
-  deriving (Eq, Show)
+  { userSignatureHost :: Text
+  , userSignatureRequestId :: UUID
+  , userSignaturePublicKey :: PublicKey
+  , userSignatureUserId :: UUID
+  } deriving (Eq, Show)
 
 
 data SessionSignature = SessionSignature
-  deriving (Eq, Show)
+  { sessionSignatureHost :: Text
+  , sessionSignatureRequestId :: UUID
+  , sessionSignaturePublicKey :: PublicKey
+  , sessionSignatureSessionId :: UUID
+  } deriving (Eq, Show)
+
+
+data CommonAuthHeaders = CommonAuthHeaders
+  { authHeader      :: ByteString
+  , hostHeader      :: ByteString
+  , publicKeyHeader :: ByteString
+  , requestIdHeader :: ByteString
+  } deriving (Eq, Show)
 
 
 type instance AuthServerData (AuthProtect "user-signature") = UserSignature
@@ -34,12 +58,66 @@ authContext = userAuthHandler :. sessionAuthHandler :. EmptyContext
 
 
 userAuthHandler :: AuthHandler Request UserSignature
-userAuthHandler = mkAuthHandler $ \_ -> do
-  liftIO $ putStrLn "TODO: Implement user auth handler"
-  return UserSignature
+userAuthHandler = mkAuthHandler $ \req -> do
+  authHeaders <- ensureCommonAuthHeaders req
+  userIdHeader <- ensureHeader req "X-CROSSMAP-User-Id"
+  checkUserAuth authHeaders userIdHeader
 
 
 sessionAuthHandler :: AuthHandler Request SessionSignature
-sessionAuthHandler = mkAuthHandler $ \_ -> do
-  liftIO $ putStrLn "TODO: Implement session auth handler"
-  return SessionSignature
+sessionAuthHandler = mkAuthHandler $ \req -> do
+  authHeaders <- ensureCommonAuthHeaders req
+  sessionIdHeader <- ensureHeader req "X-CROSSMAP-Session-Id"
+  sessionTokenHeader <- ensureHeader req "X-CROSSMAP-Session-Token"
+  checkSessionAuth authHeaders sessionIdHeader sessionTokenHeader
+
+
+ensureCommonAuthHeaders :: Request -> Handler CommonAuthHeaders
+ensureCommonAuthHeaders req = do
+  authHeader      <- ensureHeader req "Authorization"
+  hostHeader      <- ensureHeader req "Host"
+  publicKeyHeader <- ensureHeader req "X-CROSSMAP-Public-Key"
+  requestIdHeader <- ensureHeader req "X-CROSSMAP-Request-Id"
+  return CommonAuthHeaders {..}
+
+
+checkUserAuth :: CommonAuthHeaders -> ByteString -> Handler UserSignature
+checkUserAuth CommonAuthHeaders{..} userId = do
+  liftIO $ putStrLn "TODO: Implement user signature check"
+  userSignatureHost <- return $ decodeUtf8 hostHeader
+  userSignatureRequestId <- ensureValidUUID requestIdHeader
+  userSignaturePublicKey <- ensureValidPublicKey publicKeyHeader
+  userSignatureUserId <- ensureValidUUID userId
+  return UserSignature{..}
+
+
+checkSessionAuth ::
+  CommonAuthHeaders -> ByteString -> ByteString -> Handler SessionSignature
+checkSessionAuth CommonAuthHeaders{..} sessionId sessionToken = do
+  liftIO $ putStrLn "TODO: Implement session signature check"
+  sessionSignatureHost <- return $ decodeUtf8 hostHeader
+  sessionSignatureRequestId <- ensureValidUUID requestIdHeader
+  sessionSignaturePublicKey <- ensureValidPublicKey publicKeyHeader
+  sessionSignatureSessionId <- ensureValidUUID sessionId
+  return SessionSignature{..}
+
+
+ensureHeader :: Request -> HeaderName -> Handler ByteString
+ensureHeader req name = case lookup name (requestHeaders req) of
+  Just value -> return value
+  Nothing -> throwError $ err401
+    { errBody = "Missing required header: " <> cast name }
+  where
+    cast = fromStrict . original
+
+
+ensureValidUUID :: ByteString -> Handler UUID
+ensureValidUUID bs = case fromText $ decodeUtf8 bs of
+  Just uuid -> return uuid
+  Nothing -> throwError $ err401 { errBody = "Invalid UUID: " <> fromStrict bs }
+
+
+ensureValidPublicKey :: ByteString -> Handler PublicKey
+ensureValidPublicKey bs = if Data.ByteString.length bs == 32
+  then return $ PublicKey bs
+  else throwError $ err401 { errBody = "Invalid public key" }

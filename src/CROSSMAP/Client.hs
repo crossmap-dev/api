263ae769fe631ff0d7ffd1e6213203e20304b940
Mike Taylor <mike.charles.taylor@gmail.com>
2024-09-27 17:32:25 -0600

    WIP: login

--- src/CROSSMAP/Client.hs
+++ src/CROSSMAP/Client.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module CROSSMAP.Client
   ( Client(..)
   , newSession
@@ -5,12 +6,19 @@ module CROSSMAP.Client
   , runClient
   ) where
 
+import Data.Base64.Types
+import Data.ByteString.Base64
 import Data.Text
+import Data.Text.Encoding
+import Data.UUID
+import Data.UUID.V4
 import Crypto.Sign.Ed25519
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import qualified Servant.Client as SC
 
+import CROSSMAP.Client.API
+import CROSSMAP.Login
 import CROSSMAP.Password
 import CROSSMAP.PublicKey
 import CROSSMAP.SecretKey
@@ -36,30 +44,43 @@ data Client = Client
 
 newSession :: Text -> Text -> Text -> IO Client
 newSession url username password = do
+  url' <- SC.parseBaseUrl $ unpack url
   (userPK, userSK) <- keyDerivation username password
   (sessionPK, sessionSK) <- createKeypair
-  manager <- newManager tlsManagerSettings
-  url' <- SC.parseBaseUrl $ unpack url
-  return Client
-    { clientURL = url
-    , clientEnv = SC.mkClientEnv manager url'
-    , clientManager = manager
-    , clientUsername = username
-    , clientUserPublicKey = userPK
-    , clientUserSecretKey = userSK
-    , clientSessionPublicKey = sessionPK
-    , clientSessionSecretKey = sessionSK
-    }
+  let uah req = signRequest userPK userSK req
+  let sah req = signRequest sessionPK sessionSK req
+  loginManager <- newManager tlsManagerSettings { managerModifyRequest = uah }
+  let loginEnv = SC.mkClientEnv loginManager url'
+  let loginRequest = LoginRequest
+        { loginRequestUsername = username
+        , loginRequestSessionPublicKey = Base64PublicKey userPK
+        }
+  response <- SC.runClientM (loginClient loginRequest) loginEnv
+  case response of
+    Left err -> fail $ show err
+    Right _ -> do
+      manager <- newManager tlsManagerSettings { managerModifyRequest = sah }
+      return Client
+        { clientURL = url
+        , clientEnv = SC.mkClientEnv manager url'
+        , clientManager = manager
+        , clientUsername = username
+        , clientUserPublicKey = userPK
+        , clientUserSecretKey = userSK
+        , clientSessionPublicKey = sessionPK
+        , clientSessionSecretKey = sessionSK
+        }
 
 
 loadSession :: Text -> Text -> Text -> Text -> Text -> Text -> IO Client
 loadSession url username userPK userSK sessionPK sessionSK = do
   url' <- SC.parseBaseUrl $ unpack url
-  manager <- newManager tlsManagerSettings
   userPK' <- maybe (fail "Invalid user public key") return $ publicKeyFromText userPK
   userSK' <- maybe (fail "Invalid user secret key") return $ secretKeyFromText userSK
   sessionPK' <- maybe (fail "Invalid session public key") return $ publicKeyFromText sessionPK
   sessionSK' <- maybe (fail "Invalid session secret key") return $ secretKeyFromText sessionSK
+  let sah req = signRequest sessionPK' sessionSK' req
+  manager <- newManager tlsManagerSettings { managerModifyRequest = sah }
   return Client
     { clientURL = url
     , clientEnv = SC.mkClientEnv manager url'
@@ -74,3 +95,29 @@ loadSession url username userPK userSK sessionPK sessionSK = do
 
 runClient :: Client -> ClientM a -> IO (Either ClientError a)
 runClient client action = SC.runClientM action $ clientEnv client
+
+
+signRequest :: PublicKey -> SecretKey -> Request -> IO Request
+signRequest pk sk req = do
+  requestId <- nextRandom
+  let stringToSign = authStringToSign req requestId
+  let Signature signature = dsign sk $ encodeUtf8 stringToSign
+  let signatureB64 = extractBase64 $ encodeBase64 signature
+  let req' = req
+        { requestHeaders = requestHeaders req
+          <> [ ("X-CROSSMAP-Request-Id", encodeUtf8 $ toText requestId)
+             , ("X-CROSSMAP-Public-Key", encodeUtf8 $ publicKeyToText pk)
+             , ("Authorization", "Signature " <> (encodeUtf8 $ signatureB64))
+             ]
+        }
+  return req'
+
+
+authStringToSign :: Request -> UUID -> Text
+authStringToSign req reqId
+  = decodeUtf8 $ r <> " " <> m <> " " <> h <> p <> q
+  where m = method req
+        h = host req
+        p = path req
+        q = queryString req
+        r = encodeUtf8 $ toText reqId

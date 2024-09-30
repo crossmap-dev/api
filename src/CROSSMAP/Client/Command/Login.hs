98d2593d8493758fd41fc741159daff490f6e7f3
Mike Taylor <mike.charles.taylor@gmail.com>
2024-09-29 22:07:56 -0600

    Accept login password from stdin

--- src/CROSSMAP/Client/Command/Login.hs
+++ src/CROSSMAP/Client/Command/Login.hs
@@ -5,8 +5,10 @@ module CROSSMAP.Client.Command.Login
   , runLogin
   ) where
 
+import Control.Exception
 import Data.Text
 import Options.Applicative
+import System.IO
 
 import CROSSMAP.Client
 import CROSSMAP.Client.State
@@ -15,7 +17,7 @@ import CROSSMAP.Client.State
 data LoginCommand = LoginCommand
   { loginCommandURL :: Text
   , loginCommandUsername :: Text
-  , loginCommandPassword :: Text
+  , loginCommandPassword :: Maybe Text
   } deriving (Show)
 
 
@@ -23,11 +25,28 @@ loginOptions :: Parser LoginCommand
 loginOptions = LoginCommand
   <$> argument str ( metavar "URL" )
   <*> argument str ( metavar "USERNAME" )
-  <*> argument str ( metavar "PASSWORD" )
+  <*> optional (argument str ( metavar "PASSWORD" ))
 
 
 runLogin :: LoginCommand -> IO ()
 runLogin LoginCommand{..} = do
-  client <- newSession loginCommandURL loginCommandUsername loginCommandPassword
+  password <- resolvePassword loginCommandPassword
+  client <- newSession loginCommandURL loginCommandUsername password
   saveState $ clientToState client
   putStrLn "Login successful."
+
+
+resolvePassword :: Maybe Text -> IO Text
+resolvePassword Nothing = do
+  putStr "Password: "
+  hFlush stdout
+  pass <- withEcho False getLine
+  putStrLn ""
+  return $ pack pass
+resolvePassword (Just pass) = return pass
+
+
+withEcho :: Bool -> IO a -> IO a
+withEcho echo act = do
+  old <- hGetEcho stdin
+  bracket_ (hSetEcho stdin echo) (hSetEcho stdin old) act

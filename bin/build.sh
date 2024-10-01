11f45f479665c0214437352a2adfdd77d74953f0
Mike Taylor <mike.charles.taylor@gmail.com>
2024-10-01 00:30:12 -0600

    Add (generated) version file

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -euxo pipefail
+
 VERSION=$(tr -d '\n' < VERSION.txt)
+
 sed -i "s/version: .*/version: ${VERSION}/" package.yaml
+
+cat <<EOF > src/CROSSMAP/Version.hs
+{-# LANGUAGE OverloadedStrings #-}
+module CROSSMAP.Version ( version ) where
+import Data.Text ( Text )
+version :: Text
+version = "${VERSION}"
+EOF
+
 hpack
+
 cabal install --installdir=target --install-method=copy --overwrite-policy=always

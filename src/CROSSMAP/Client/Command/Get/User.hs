097e1b508f4edd16a7df02a6c6e071cef3b117c6
Mike Taylor <mike.charles.taylor@gmail.com>
2024-10-01 02:23:21 -0600

    Implement get user command

--- src/CROSSMAP/Client/Command/Get/User.hs
+++ src/CROSSMAP/Client/Command/Get/User.hs
@@ -1,11 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module CROSSMAP.Client.Command.Get.User
   ( GetUserCommand(..)
   , getUserOptions
   , runGetUser
   ) where
 
-import Data.Text (Text)
+import Data.Text (Text, unpack)
+import Data.UUID (toText)
 import Options.Applicative
+
+import CROSSMAP.Client
+import CROSSMAP.Client.API
+import CROSSMAP.Client.State
+import CROSSMAP.PublicKey
+import CROSSMAP.User
 
 
 data GetUserCommand = GetUserCommand
@@ -20,4 +29,24 @@ getUserOptions = GetUserCommand
 
 runGetUser :: GetUserCommand -> IO ()
 runGetUser (GetUserCommand username) = do
-  putStrLn $ "Getting user: " ++ show username
+  maybeState <- loadState
+  case maybeState of
+    Nothing ->
+      putStrLn "Client not logged in."
+    Just state -> do
+      client <- loadSessionFromState state
+      result <- runClient client $ getUserByUsernameClient username
+      case result of
+        Left err ->
+          putStrLn $ "Error: " ++ show err
+        Right user -> do
+          putStrLn $ "User ID: " ++ unpack (toText (userResponseUserId user))
+          putStrLn "Usernames:"
+          mapM_ (putStrLn . unpack . ("- " <>)) (userResponseUsernames user)
+          putStrLn "Public keys:"
+          mapM_ putPublicKey (userResponsePublicKeys user)
+  where
+    putPublicKey UserPublicKey{..} = do
+      putStrLn $ "- Public key: " ++ unpack (base64PublicKeyToText userPublicKey)
+      putStrLn $ "  Created at: " ++ show userPublicKeyCreatedAt
+      putStrLn $ "  Expires at: " ++ show userPublicKeyExpiresAt

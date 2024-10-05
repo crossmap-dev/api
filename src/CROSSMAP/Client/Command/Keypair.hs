2bfe4dd2e0010e2da9d6b8a0a889d9a5773f5b1c
Mike Taylor <mike.charles.taylor@gmail.com>
2024-10-04 23:14:44 -0600

    Add flag to register keypair

--- src/CROSSMAP/Client/Command/Keypair.hs
+++ src/CROSSMAP/Client/Command/Keypair.hs
@@ -5,19 +5,26 @@ module CROSSMAP.Client.Command.Keypair
   , runKeypair
   ) where
 
+import Control.Monad
 import Crypto.Sign.Ed25519
 import Data.Base64.Types
 import Data.ByteString.Base64.URL
 import Data.Text
 import Options.Applicative
 
+import CROSSMAP.Client
+import CROSSMAP.Client.API
+import CROSSMAP.Client.State
 import CROSSMAP.Client.Util
 import CROSSMAP.Password
+import CROSSMAP.PublicKey
+import CROSSMAP.User
 
 
 data KeypairCommand = KeypairCommand
   { keypairCommandUsername :: Text
   , keypairCommandPassword :: Maybe Text
+  , keypairCommandRegister :: Bool
   } deriving (Show)
 
 
@@ -25,6 +32,7 @@ keypairOptions :: Parser KeypairCommand
 keypairOptions = KeypairCommand
   <$> argument str ( metavar "USERNAME" )
   <*> optional (argument str ( metavar "PASSWORD" ))
+  <*> switch ( long "register" <> short 'r' <> help "Register the keypair" )
 
 
 runKeypair :: KeypairCommand -> IO ()
@@ -33,3 +41,29 @@ runKeypair KeypairCommand{..} = do
   (pk, sk) <- keyDerivation keypairCommandUsername password
   putStrLn $ "Public key: " ++ (unpack $ extractBase64 $ encodeBase64 $ unPublicKey pk)
   putStrLn $ "Secret key: " ++ (unpack $ extractBase64 $ encodeBase64 $ unSecretKey sk)
+  when keypairCommandRegister $ do
+    maybeState <- loadState
+    case maybeState of
+      Nothing ->
+        putStrLn "Client not logged in."
+      Just state -> do
+        client <- loadSessionFromState state
+        getUserResult <- runClient client getUserClient
+        case getUserResult of
+          Left err -> do
+            putStrLn $ "Error: " ++ show err
+            return ()
+          Right user -> do
+            -- ensure the username is valid
+            if (keypairCommandUsername `Prelude.elem` userResponseUsernames user)
+              then do
+                let req = CreatePublicKeyRequest pk Nothing
+                createPublicKeyResult <- runClient client $ createPublicKeyClient req
+                case createPublicKeyResult of
+                  Left err ->
+                    putStrLn $ "Error: " ++ show err
+                  Right _ ->
+                    putStrLn "Keypair registered."
+              else do
+                putStrLn $ "Error: " ++ unpack keypairCommandUsername ++ " is not logged in."
+                return ()

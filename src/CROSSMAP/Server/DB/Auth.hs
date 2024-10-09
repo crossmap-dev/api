b8ef6bbea87a620c6bebf8db54d9135e5d5f2360
Mike Taylor <mike.charles.taylor@gmail.com>
2024-10-08 20:56:52 -0600

    Implement authorization query

--- src/CROSSMAP/Server/DB/Auth.hs
+++ src/CROSSMAP/Server/DB/Auth.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 module CROSSMAP.Server.DB.Auth
-  ( PublicKeyInfoPolicyRow(..)
+  ( AuthorizationResult(..)
+  , authorizationQuery
+  , PublicKeyInfoPolicyRow(..)
   , publicKeyInfoPolicyQuery
   ) where
 
@@ -8,6 +10,7 @@ import Crypto.Sign.Ed25519 (PublicKey(..))
 import Data.Functor.Contravariant ((>$<))
 import Data.Text (Text)
 import Data.Time.Clock (UTCTime)
+import Data.UUID (UUID)
 import Hasql.Statement (Statement(..))
 import Hasql.Transaction (Transaction, statement)
 import qualified Hasql.Encoders as E
@@ -18,10 +21,48 @@ import CROSSMAP.PublicKey
 import CROSSMAP.User (UserId(..))
 
 
+data AuthorizationResult = AuthorizationResult
+  { authorizationResultPolicyId :: PolicyId
+  , authorizationResultRuleUuid :: UUID
+  } deriving (Eq, Show)
+
+
 data PublicKeyInfoPolicyRow = PublicKeyInfoPolicyRow
   { publicKeyInfoPolicyRowPublicKeyInfo :: PublicKeyInfo
   , publicKeyInfoPolicyRowPolicyIds :: [PolicyId]
   } deriving (Eq, Show)
+
+
+authorizationQuery ::
+  [PolicyId] -> Text -> Bool -> Transaction (Maybe AuthorizationResult)
+authorizationQuery policyIds resource isWrite =
+  statement (policyIds, resource, isWrite) authorizationQueryStatement
+
+
+authorizationQueryStatement ::
+  Statement ([PolicyId], Text, Bool) (Maybe AuthorizationResult)
+authorizationQueryStatement = Statement sql encoder decoder True where
+  sql = "SELECT \
+        \  pr.policy_uuid, \
+        \  pr.rule_uuid \
+        \FROM unnest($1::uuid[]) AS p(policy_uuid) \
+        \JOIN LATERAL find_matching_policies($2, $3, ARRAY[p.policy_uuid]) mpr ON true \
+        \JOIN policies_rules pr \
+        \  ON mpr.policy_uuid = pr.policy_uuid \
+        \  AND mpr.rule_uuid = pr.rule_uuid \
+        \GROUP BY pr.policy_uuid, pr.rule_uuid \
+        \LIMIT 1"
+  encoder :: E.Params ([PolicyId], Text, Bool)
+  encoder
+    = ((\(pids, _, _) -> pids) >$< policyIdsEncoder)
+    <> ((\(_, r, _) -> r) >$< E.param (E.nonNullable E.text))
+    <> ((\(_, _, w) -> w) >$< E.param (E.nonNullable E.bool))
+  decoder = D.rowMaybe $ AuthorizationResult
+    <$> (PolicyId <$> D.column (D.nonNullable D.uuid))
+    <*> D.column (D.nonNullable D.uuid)
+  policyIdsEncoder
+    = f >$< E.param (E.nonNullable (E.foldableArray (E.nonNullable E.uuid)))
+    where f = map unPolicyId
 
 
 publicKeyInfoPolicyQuery :: PublicKey -> Transaction (Maybe PublicKeyInfoPolicyRow)

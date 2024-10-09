64e6c6d48a062cc466fa2ddd02da373b278f06e9
Mike Taylor <mike.charles.taylor@gmail.com>
2024-10-09 16:17:24 -0600

    Fix create group handler

--- src/CROSSMAP/Server/Handlers/Group.hs
+++ src/CROSSMAP/Server/Handlers/Group.hs
@@ -43,7 +43,7 @@ getGroupsHandler state@State{..} signatureInfo = do
 createGroupHandler :: State -> SignatureInfo -> CreateGroupRequest -> Handler Group
 createGroupHandler state@State{..} signatureInfo CreateGroupRequest{..} = do
   _ <- authorize state signatureInfo
-  result0 <- liftIO $ runQuery pool $ resolveUsers createGroupUsers
+  result0 <- liftIO $ runUpdate pool $ resolveUsers createGroupUsers
   case result0 of
     Left err -> liftIO (print err) >> throwError err500 { errBody = "Database error" }
     Right (Left err) -> throwError err400 { errBody = fromStrict $ encodeUtf8 err }

dd40e2a81bbc644abe2bcd2ad4316c841e4fe024
Mike Taylor <mike.charles.taylor@gmail.com>
2024-10-12 22:42:21 -0600

    Refactor key derivation

--- src/CROSSMAP/Password.hs
+++ src/CROSSMAP/Password.hs
@@ -1,28 +1,42 @@
 module CROSSMAP.Password
   ( keyDerivation
+  , seedDerivation
+  , usernameSalt
   ) where
 
 import Crypto.Error
 import Crypto.Hash
 import Crypto.Sign.Ed25519
 import Crypto.KDF.Argon2
+import Data.ByteArray
+import Data.ByteString
 import Data.Text
 import Data.Text.Encoding
 
 
 keyDerivation :: Text -> Text -> IO (PublicKey, SecretKey)
 keyDerivation username password = do
-  let pass = encodeUtf8 password
-  let salt = hashWith SHA256 (encodeUtf8 username)
-  seed <- throwCryptoErrorIO $ Crypto.KDF.Argon2.hash options pass salt 32
+  seed <- seedDerivation username password
   case createKeypairFromSeed_ seed of
     Nothing -> error "Failed to create keypair"
     Just (pk, sk) -> return (pk, sk)
+
+
+seedDerivation :: Text -> Text -> IO ByteString
+seedDerivation username password = do
+  let pass = encodeUtf8 password
+  let salt = usernameSalt username
+  throwCryptoErrorIO $ Crypto.KDF.Argon2.hash options pass salt 32
   where
     options = Options
-      { iterations = 2
-      , memory = 2 ^ (23 :: Int)
-      , parallelism = 4
+      { iterations = 4
+      , memory = 2 ^ (20 :: Int)
+      , parallelism = 1
       , variant = Argon2id
       , version = Version13
       }
+
+
+usernameSalt :: Text -> ByteString
+usernameSalt username =
+  Data.ByteArray.take 16 $ Data.ByteArray.convert $ hashWith SHA256 $ encodeUtf8 username
